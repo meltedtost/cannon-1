488b8aeb569e86cbeeacd8d5e361231a620969a0
meltedtost <90787832+meltedtost@users.noreply.github.com>
2021-11-17 17:48:50 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# cannon-1
+# PRO-C22-SA-Boilerplate_code
+Boilerplate code for student  
---
+++ js/Cannon.js
@@ -0,0 +1,20 @@
+class Cannon {
+  constructor(x, y, width, height, angle) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.angle = angle;
+    this.cannon_image=loadImage("assets/canon.png")
+    this.cannon_Base=loadImage("assets/cannonBase.png")
+  }
+display(){
+  push()
+  translate(this.x,this.y)
+  rotate(this.angle)
+  imageMode(CENTER)
+  image(this.cannon_image,0,0,this.width,this.height)
+  pop()
+  image(this.cannon_Base,70,20,200,200)
+}
+}
